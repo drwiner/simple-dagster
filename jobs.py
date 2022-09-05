67507bcf2e9add7f05a487826787d076a11db1f3
David Winer <davidwiner@Davids-MacBook-Pro.local>
2022-09-05 15:00:12 -0400

    graphs

--- jobs.py
+++ jobs.py
@@ -6,18 +6,36 @@ def get_config(context):
     context.log.info("config: " + str(context.op_config))
     return context.op_config
 
-@dagster.op
+
+@dagster.op(config_schema={"key": str})
 def using_config(get_config):
+    # print("ok")
     for key, value in get_config.items():
         print(f"using {key} {value}")
+
+    return True
+
+@dagster.op
+def using_config_2(get_config):
+    # print("ok")
+    for key, value in get_config.items():
+        print(f"using again {key} {value}")
+    return True
+
+@dagster.op
+def combine(context, a: bool, b: bool):
+    print("combined")
 
 
 @dagster.job
 def do_configured_job():
-    using_config(get_config())
+    x = get_config()
+    a = using_config(x)
+    b = using_config_2(x)
+    combine(a, b)
 
 
 if __name__ == "__main__":
     do_configured_job.execute_in_process(
-        run_config={"ops": {"get_config": {"config": {"env": "dev", "application": "do_stuff"}}}}
+        run_config={"ops": {"using_config": {"config": {"key": "value"}}, "get_config": {"config": {"env": "dev", "application": "do_stuff"}}}}
     )
